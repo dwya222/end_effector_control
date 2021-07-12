f91ce9010431dd6aa464a572249417795f2fe3a7
David Yackzan <dwyackzan@gmail.com>
2021-07-12 09:47:02 -0400

    Clean up code and add dynamic capabilities

--- end_effector_control/scripts/cmd_point_publisher.py
+++ end_effector_control/scripts/cmd_point_publisher.py
@@ -1,10 +1,10 @@
 import rospy
 from geometry_msgs.msg import Point
 import geometry_msgs.msg
-point_publisher = rospy.Publisher('/point_command', Point, queue_size=10)
-rospy.init_node('point_pub')
+point_publisher = rospy.Publisher('/point_command', Point, queue_size=1)
+rospy.init_node('point_pub', anonymous=True)
 point_goal = geometry_msgs.msg.Point()
-point_goal.x = 0.7
+point_goal.x = 0.5
 point_goal.y = 0.0
 point_goal.z = 0.4
 point_publisher.publish(point_goal)
--- end_effector_control/scripts/sample.py
+++ end_effector_control/scripts/sample.py
@@ -1,43 +1,8 @@
 #!/usr/bin/env python
 
-import sys
-import copy
-import rospy
-import moveit_commander
-import moveit_msgs.msg
-import geometry_msgs.msg
-from math import pi
-from std_msgs.msg import String
-from moveit_commander.conversions import pose_to_list
 import numpy as np
 from scipy.spatial.transform import Rotation as R
-
-
-class MoveGroupPythonIntefaceTutorial(object):
-  """MoveGroupPythonIntefaceTutorial"""
-  def __init__(self):
-    super(MoveGroupPythonIntefaceTutorial, self).__init__()
-    moveit_commander.roscpp_initialize(sys.argv)
-    rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
-    robot = moveit_commander.RobotCommander()
-    scene = moveit_commander.PlanningSceneInterface()
-    group_name = "panda_arm"
-    move_group = moveit_commander.MoveGroupCommander(group_name)
-    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
-                                                   moveit_msgs.msg.DisplayTrajectory,
-                                                   queue_size=20)
-    planning_frame = move_group.get_planning_frame()
-    eef_link = move_group.get_end_effector_link()
-    group_names = robot.get_group_names()
-
-    self.box_name = ''
-    self.robot = robot
-    self.scene = scene
-    self.move_group = move_group
-    self.display_trajectory_publisher = display_trajectory_publisher
-    self.planning_frame = planning_frame
-    self.eef_link = eef_link
-    self.group_names = group_names
+from demo_interface import DemoInterface
 
 def euler(pose):
     new_rot = R.from_quat([pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w])
@@ -45,7 +10,7 @@ def euler(pose):
     return output
 
 def main():
-  mover = MoveGroupPythonIntefaceTutorial()
+  mover = DemoInterface()
   np.set_printoptions(suppress=True)
   while True:
        raw_input("Type enter to see next pose \n")
